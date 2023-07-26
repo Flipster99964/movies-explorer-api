6dc9e0f76325bc989b36cf70e9020bc041306af3
Flipster99964 <lay.12345@yandex.ru>
2023-07-26 15:09:37 +0300

    add app, express

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/moviesdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.post('/signup', createUser);
+app.post('/signin', login);
+
+app.use('/users', auth, userRouter);
+app.use('/movies', auth, movieRouter);
+app.use(handleErrors);
+
+
+app.listen(PORT, () => {
+    // Если всё работает, консоль покажет, какой порт приложение слушает
+    console.log(`App listening on port ${PORT}`)
+}) 
